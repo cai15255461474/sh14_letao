e17e59818f20b9b596ccfbec16d87bec71a6516b
taoge <1468012377@qq.com>
2017-10-31 16:51:21 +0800

    登陆完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,19 @@
 /**
  * Created by 蔡成涛 on 2017/10/30.
  */
+$(document).ajax(function(){
+    // 让进度条显示
+    NProgress.start();
+
+
+})
+
+$(document).ajaxStop(function(){
+    setTimeout(function(){
+        // 让进度条结束
+        NProgress.done();
+    },500);
+
+})
+
+
